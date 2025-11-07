5549260760ba18a1a8f7a6f97dd55cfb37900f39
Matthew Musoke <matthew@talkatoo.com>
2025-11-07 10:59:31 -0400

    Remove warning

--- app/VideoPlayer/Models/VideoModel.swift
+++ app/VideoPlayer/Models/VideoModel.swift
@@ -17,7 +17,7 @@ struct VideoModel: Identifiable, Codable {
     let author: AuthorModel
 
     var publishedAtDate: Date? {
-        var dateFormatter = DateFormatter()
+        let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
         return dateFormatter.date(from: publishedAt)
     }
